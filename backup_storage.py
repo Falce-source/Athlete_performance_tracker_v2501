8d4ff12017950861978ae28b719c925663decb07
Andrés <andres.falceto@gmail.com>
2025-11-10 15:02:26 +0100

    Update backup_storage.py

--- backup_storage.py
+++ backup_storage.py
@@ -31,7 +31,6 @@ def _get_service():
 
     try:
         creds = Credentials(
-            token=None,
             refresh_token=refresh_token,
             client_id=client_id,
             client_secret=client_secret,
